f34867a6e73f5c3d271c5c8845239407e261f943
Nekich06 <p.nikita.73526@gmail.com>
2024-10-16 13:42:49 +0300

    Added some realizations in matrix.cpp; added declarators in matrix.hpp and functions in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main()
 	std::cin >> M >> N;
 	std::cout << M << " " << N << "\n";
 	int ** t = create_matrix(M, N);
+	input_matrix(t, M, N);
+	output_matrix(t, M, N);
 	del_matrix(t, M, N);
 }
 
--- matrix.cpp
+++ matrix.cpp
@@ -1,2 +1,19 @@
+#include <iostream>
 #include "matrix.hpp"
+void input_matrix(int ** t, size_t m, size_t n) {
+	for (size_t i = 0; i < M; ++i) {
+		for (size_t j = 0; j < N; ++i) {
+			std::cin >> t[i][j];
+		}
+	}
+}
+void output_matrix(const int * const * t, size_t m, size_t n) {
+	for (size_t i = 0; i < M; ++i) {
+		std::cout << t[i][0];
+		for (size_t j = 0; j < m; ++i) {
+			std::cout << " " << t[i][j];
+			std::cout << "\n";
+		}
+	}
+}
 
