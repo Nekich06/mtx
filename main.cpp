136d4b0ab2abc3eb6ad971b7b1555693634ef1d0
Nekich06 <p.nikita.73526@gmail.com>
2024-10-16 13:06:09 +0300

    Added inicializing of matrix and declarators of two functions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+
+int ** create_matrix(size_t m, size_t n);
+void del_matrix(int ** t, size_t m, size_t n);
+
 int main()
 {
 	size_t M = 0, N = 0;
 	std::cin >> M >> N;
 	std::cout << M << " " << N << "\n";
+	int ** t = create_matrix(M, N);
+	del_matrix(t, M, N);
 }
+
