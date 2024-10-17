aa84a635454575c64d0e6dac031afe214ddd5e1d
Nekich06 <p.nikita.73526@gmail.com>
2024-10-18 01:25:21 +0300

    Modified tabulation according to Coding Guidelines

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 
 int main()
 {
-	size_t M = 0, N = 0;
-	std::cin >> M >> N;
-	std::cout << M << " " << N << "\n";
-	int ** t = create_matrix(M, N);
-	input_matrix(t, M, N);
-	output_matrix(t, M, N);
-	del_matrix(t, M, N);
+  size_t M = 0, N = 0;
+  std::cin >> M >> N;
+  std::cout << M << " " << N << "\n";
+  int ** t = create_matrix(M, N);
+  input_matrix(t, M, N);
+  output_matrix(t, M, N);
+  del_matrix(t, M, N);
 }
 
--- matrix.cpp
+++ matrix.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include "matrix.hpp"
 void input_matrix(int ** t, size_t m, size_t n) {
-	for (size_t i = 0; i < M; ++i) {
-		for (size_t j = 0; j < N; ++i) {
-			std::cin >> t[i][j];
-		}
-	}
+  for (size_t i = 0; i < M; ++i) {
+    for (size_t j = 0; j < N; ++i) {
+      std::cin >> t[i][j];
+      }
+  }
 }
 void output_matrix(const int * const * t, size_t m, size_t n) {
-	for (size_t i = 0; i < M; ++i) {
-		std::cout << t[i][0];
-		for (size_t j = 0; j < m; ++i) {
-			std::cout << " " << t[i][j];
-			std::cout << "\n";
-		}
-	}
+  for (size_t i = 0; i < M; ++i) {
+  std::cout << t[i][0];
+    for (size_t j = 0; j < m; ++i) {
+      std::cout << " " << t[i][j];
+      std::cout << "\n";
+      }
+  }
 }
 void del_matrix(int ** t, size_t m) {
-	for (size_t i = 0; i < M; ++i) {
-		delete[] t[i];
-	}
-	delete[] t;
+  for (size_t i = 0; i < M; ++i) {
+    delete[] t[i];
+  }
+  delete[] t;
 }
 
